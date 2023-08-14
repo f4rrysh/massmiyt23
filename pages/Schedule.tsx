751485488402b8904a0feff5b9aabcc65962a39e
f4rrysh <131455435+f4rrysh@users.noreply.github.com>
2023-08-14 22:12:56 +0800

    feat: schedule page

---
+++ pages/Schedule.tsx
@@ -0,0 +1,18 @@
+import React, { lazy } from 'react';
+import { render } from 'utils/render';
+
+// App
+import App from 'app/App';
+
+// Styling
+import 'styles/pages/Schedule.scss';
+
+function Schedule(): JSX.Element {
+    return (
+        <App>
+            <div className="schedule"></div>
+        </App>
+    );
+}
+
+render(<Schedule />);
