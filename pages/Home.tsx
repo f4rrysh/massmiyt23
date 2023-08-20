16771d2a332cc193ff3841d99f17f907c29e2598
f4rrysh <131455435+f4rrysh@users.noreply.github.com>
2023-08-21 02:29:21 +0800

    feat: image

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -8,13 +8,19 @@ import App from 'app/App';
 const Fixture = lazy(() => import('layouts/Fixture'));
 const Rank = lazy(() => import('layouts/Rank'));
 
+import Image from 'react-bootstrap/Image';
+
 // import sekolah from 'images/sekolah.jpg';
 
 function Home(): JSX.Element {
     return (
         <App title="Home">
             <div className="home">
+                <Image src="sekolah.jpg" fluid />
                 <Fixture />
+                <br />
+
+                <h3>Standing</h3>
                 <Rank />
             </div>
         </App>
--- pages/Result.tsx
+++ pages/Result.tsx
@@ -4,15 +4,12 @@ import { render } from 'utils/render';
 // App
 import App from 'app/App';
 
-const Rank = lazy(() => import('layouts/Rank'));
-
 function Result(): JSX.Element {
     return (
         <App title="Result">
             <div className="result">
                 <h3>Result</h3>
                 <p>Get up-to-date result of the played matches</p>
-                <Rank />
             </div>
         </App>
     );
