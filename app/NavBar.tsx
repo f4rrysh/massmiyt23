a397707e684d0495bc80f9f8122c1dcd1d83fbcf
f4rrysh <131455435+f4rrysh@users.noreply.github.com>
2023-08-21 11:23:37 +0800

    feat: v1.1

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Image from 'react-bootstrap/Image';
 
 import Menu from 'images/menu.svg';
 
@@ -11,7 +12,16 @@ export default function NavBar(): JSX.Element {
         <header>
             <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
                 <Container fluid>
-                    <Navbar.Brand href="/">MASSMIYT23</Navbar.Brand>
+                    <Navbar.Brand href="/">
+                        <Image
+                            src="logo.jpg"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            roundedCircle
+                        />{' '}
+                        MASSMIYT23
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
